cef2e7ba3aae97ff02de1874c8d8d3c9e1291776
Frederic St-Onge <frederic.st-onge@mail.mcgill.ca>
2020-05-27 14:14:48 -0400

    Created mnc2bids script
    Functional, but specific to the current dataset

--- 01_script_conversion_mnc2bids/mnc2bids.sh
+++ 01_script_conversion_mnc2bids/mnc2bids.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
 
-set -e #Setting the environment
-toplvl=/Users/stong3/Desktop/bhs_project #The top level of our folder
-mncdir=/Users/stong3/Desktop/bhs_project/sourcedata #Where our mnc data is stored
-#The mncdir, after sourcedata, has the structure: /subject/visit/images
-mnc2niidir=/Users/stong3/Desktop/bhs_project/rawdata
+cd /files_to_convert
 
+for folders in */; do
+    subj="${folders%/}" #This line takes the directory and strips it of it's "/" so it we get a subject number (subj)
+    echo "Processing subject ${subj}"
 
-#First pass - Baseline anatomical image
-for subj in mncdir; do
-    echo "Processing subject $subj"
+    #We need to prepare new directories for the new BIDS formatted files. 
+    #The format is: rawdata/<subject>/<session>/<modality>
+    mkdir -p /converted_files/sub-${subj}/ses-1/anat #We prepare the new directories according to bids specification.
+    echo "...Directory for ${subj} converted anat BL created (session 1)"
+    mkdir -p /converted_files/sub-${subj}/ses-2/anat
+    echo "...Directory for ${subj} converted anat FU12 created (session 2)"
+    mkdir -p /converted_files/sub-${subj}/ses-1/func
+    echo "...Directory for ${subj} converted func BL created (session 1)"
+    mkdir -p /converted_files/sub-${subj}/ses-2/func
+    echo "...Directory for ${subj} converted func FU12 created (session 2)"
 
-    mkdir -p ${mnc2niidir}/sub-${subj}/ses-1/anat
-    echo "...Directory for converted anat created"
-    mkdir -p ${mnc2niidir}/sub-${subj}/ses-1/func
-    echo "...Directory for converted func created"
+    echo "Conversion - Baseline Structural"
+    # The dataset includes visit from 2 different visit labels, either PRE or NAP. Since the code still converts the data even if an error is thrown, I run the unelegant script below where every subject is tried with all labels.
+    # Since the mnc2nii function doesn't output anything if an error occurs and that the name of the file would be the same with either labels, it seems sufficient at this time.
+    mnc2nii -nii ${subj}/PREBL00/images/preventad_${subj}_PREBL00_t1w_001_t1w-defaced_001.mnc /converted_files/sub-${subj}/ses-1/anat/sub-${subj}_ses-1_T1w.nii
+    mnc2nii -nii ${subj}/NAPBL00/images/preventad_${subj}_NAPBL00_t1w_001_t1w-defaced_001.mnc /converted_files/sub-${subj}/ses-1/anat/sub-${subj}_ses-1_T1w.nii
 
-    docker run -it -d --rm \
-    -v {mncdir}/${subj}/*BL00/images:/files_to_convert:ro \
-    -v {mnc2niidir}/sub-${subj}/ses-1/anat:/converted_files_anat \
-    -v {mnc2niidir}/sub-${subj}/ses-1/func:/converted_files_func \
-    -e ${subj} \
-    nistmni/minc-toolkit \
-    mnc2nii -nii /files_to_convert/preventad_${subj}_*BL00_t1w*.mnc /converted_files_anat/sub-${subj}_ses-1_T1w.nii \
-    mnc2nii -nii /files_to_convert/preventad_${subj}_*BL00_bold_001.mnc /converted_files_func/sub-${subj}_ses-1_task-rest_acq_bold.nii
+    echo "Conversion - 12 months Structural"
+    mnc2nii -nii ${subj}/PREFU12/images/preventad_${subj}_PREFU12_t1w_001_t1w-defaced_001.mnc /converted_files/sub-${subj}/ses-2/anat/sub-${subj}_ses-2_T1w.nii
+    mnc2nii -nii ${subj}/NAPFU12/images/preventad_${subj}_NAPFU12_t1w_001_t1w-defaced_001.mnc /converted_files/sub-${subj}/ses-2/anat/sub-${subj}_ses-2_T1w.nii
 
-    ls {mnc2niidir}/{$subj}
+    echo "Conversion - Baseline Functional"
+    mnc2nii -nii ${subj}/PREBL00/images/preventad_${subj}_PREBL00_bold_001.mnc /converted_files/sub-${subj}/ses-1/func/sub-${subj}_ses-1_task-rest_acq_bold.nii
+    mnc2nii -nii ${subj}/NAPBL00/images/preventad_${subj}_NAPBL00_bold_001.mnc /converted_files/sub-${subj}/ses-1/func/sub-${subj}_ses-1_task-rest_acq_bold.nii
 
+    echo "Conversion - 12 months Functional"
+    mnc2nii -nii ${subj}/PREFU12/images/preventad_${subj}_PREFU12_bold_001.mnc /converted_files/sub-${subj}/ses-2/func/sub-${subj}_ses-2_task-rest_acq_bold.nii
+    mnc2nii -nii ${subj}/NAPFU12/images/preventad_${subj}_NAPFU12_bold_001.mnc /converted_files/sub-${subj}/ses-2/func/sub-${subj}_ses-2_task-rest_acq_bold.nii
+    
+    echo "Conversion done"
+
+done
