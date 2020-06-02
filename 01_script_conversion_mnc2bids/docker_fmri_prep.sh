0840729c95ea647cf20770e2948c2c3c8d702532
Frederic St-Onge <frederic.st-onge@mail.mcgill.ca>
2020-06-02 19:45:14 -0400

    Updated all documents. New Nilearn Script

---
+++ 01_script_conversion_mnc2bids/docker_fmri_prep.sh
@@ -0,0 +1,14 @@
+docker run -it --rm \
+    -v /Users/stong3/Desktop/test_fmriprep_PAD/sourcedata:/data:ro \
+    -v /Users/stong3/Desktop/test_fmriprep_PAD/fmriprep:/out \
+    -v /Users/stong3/Desktop/test_fmriprep_PAD/fs_license/license.txt:/opt/freesurfer/license.txt \
+    -v /Users/stong3/Desktop/test_fmriprep_PAD/work_dir:/work \
+    poldracklab/fmriprep:latest \
+    /data /out/fmriprep \
+    participant \
+    --participant-label sub-MTL0002 \
+    -w /work \
+    --low-mem \
+    --output-spaces T1w \
+    --write-graph \
+    --fs-license-file /opt/freesurfer/license.txt
--- 01_script_conversion_mnc2bids/mnc2bids.sh
+++ 01_script_conversion_mnc2bids/mnc2bids.sh
@@ -1,40 +1,74 @@
 #!/bin/bash
 
+#First, inside the container, we need to move to the directory where the files to convert are stored. We can force this with a cd command.
+
 cd /files_to_convert
 
-for folders in */; do
-    subj="${folders%/}" #This line takes the directory and strips it of it's "/" so it we get a subject number (subj)
-    echo "Processing subject ${subj}"
-
+#The loop starts below
+for folders in */; do #This starts the loop. For each folder in the files_to_convert directory, do the following:
+    subj="${folders%/}" #Take the name of the folder, strip the "/" and assign this value to a variable "subj" (subject)
+    echo "--------------------------"
+    echo "--------------------------"
+    echo "Processing subject ${subj}" #This is for us: it simply echoes which subject we are currently processing in ther terminal.
+    echo "--------------------------"
     #We need to prepare new directories for the new BIDS formatted files. 
-    #The format is: rawdata/<subject>/<session>/<modality>
-    mkdir -p /converted_files/sub-${subj}/ses-1/anat #We prepare the new directories according to bids specification.
+    #The format according to BIDS specification is: rawdata/<subject>/<session>/<modality>
+        #The loop will take care of the "subject" part using the $subj variable
+        #We create 2 sub-directories for each subject (session 1 - baseline, and session 2 - 12 months)
+        #In each sub-directory, for each session, we create an anat and a func folder
+    echo "--------------------------"
+    echo "Creating..."
+    mkdir -p /converted_files/sub-${subj}/ses-BL00/anat
     echo "...Directory for ${subj} converted anat BL created (session 1)"
-    mkdir -p /converted_files/sub-${subj}/ses-2/anat
+    mkdir -p /converted_files/sub-${subj}/ses-FU12/anat
     echo "...Directory for ${subj} converted anat FU12 created (session 2)"
-    mkdir -p /converted_files/sub-${subj}/ses-1/func
+    mkdir -p /converted_files/sub-${subj}/ses-BL00/func
     echo "...Directory for ${subj} converted func BL created (session 1)"
-    mkdir -p /converted_files/sub-${subj}/ses-2/func
+    mkdir -p /converted_files/sub-${subj}/ses-FU12/func
     echo "...Directory for ${subj} converted func FU12 created (session 2)"
 
+    echo " "
+    echo "Starting conversion..."
+    echo " "
+    #We have a little problem with the visit labels. The dataset includes visit from 2 different visit labels, either PRE or NAP. We can use a bash operator "||" to lauch
+    ## the same task with NAP instead of PRE if the mnc2nii cannot find a PRE visit.
+    echo "--------------------------------"
     echo "Conversion - Baseline Structural"
-    # The dataset includes visit from 2 different visit labels, either PRE or NAP. Since the code still converts the data even if an error is thrown, I run the unelegant script below where every subject is tried with all labels.
-    # Since the mnc2nii function doesn't output anything if an error occurs and that the name of the file would be the same with either labels, it seems sufficient at this time.
-    mnc2nii -nii ${subj}/PREBL00/images/preventad_${subj}_PREBL00_t1w_001_t1w-defaced_001.mnc /converted_files/sub-${subj}/ses-1/anat/sub-${subj}_ses-1_T1w.nii
-    mnc2nii -nii ${subj}/NAPBL00/images/preventad_${subj}_NAPBL00_t1w_001_t1w-defaced_001.mnc /converted_files/sub-${subj}/ses-1/anat/sub-${subj}_ses-1_T1w.nii
+    echo "--------------------------------"
 
+    echo "Testing if the subject has a PRE visit"
+        #If the line below fails, the || operator executes the next line. Since we use line break operators, the echo and next mnc2nii commands count as a single command.
+    mnc2nii -nii ${subj}/PREBL00/images/preventad_${subj}_PREBL00_t1w_001_t1w-defaced_001.mnc /converted_files/sub-${subj}/ses-BL00/anat/sub-${subj}_ses-BL00_T1w.nii || \
+    ( echo "There is no PRE visit. Checking for a NAP visit..." && \
+    mnc2nii -nii ${subj}/NAPBL00/images/preventad_${subj}_NAPBL00_t1w_001_t1w-defaced_001.mnc /converted_files/sub-${subj}/ses-BL00/anat/sub-${subj}_ses-BL00_T1w.nii )
+
+    echo "--------------------------------"
     echo "Conversion - 12 months Structural"
-    mnc2nii -nii ${subj}/PREFU12/images/preventad_${subj}_PREFU12_t1w_001_t1w-defaced_001.mnc /converted_files/sub-${subj}/ses-2/anat/sub-${subj}_ses-2_T1w.nii
-    mnc2nii -nii ${subj}/NAPFU12/images/preventad_${subj}_NAPFU12_t1w_001_t1w-defaced_001.mnc /converted_files/sub-${subj}/ses-2/anat/sub-${subj}_ses-2_T1w.nii
+    echo "--------------------------------"
 
+    echo "Testing if the subject has a PRE visit"
+    mnc2nii -nii ${subj}/PREFU12/images/preventad_${subj}_PREFU12_t1w_001_t1w-defaced_001.mnc /converted_files/sub-${subj}/ses-FU12/anat/sub-${subj}_ses-FU12_T1w.nii || \
+    ( echo "There is no PRE visit. Checking for a NAP visit..." && \
+    mnc2nii -nii ${subj}/NAPFU12/images/preventad_${subj}_NAPFU12_t1w_001_t1w-defaced_001.mnc /converted_files/sub-${subj}/ses-FU12/anat/sub-${subj}_ses-FU12_T1w.nii )
+
+    echo "--------------------------------"
     echo "Conversion - Baseline Functional"
-    mnc2nii -nii ${subj}/PREBL00/images/preventad_${subj}_PREBL00_bold_001.mnc /converted_files/sub-${subj}/ses-1/func/sub-${subj}_ses-1_task-rest_acq_bold.nii
-    mnc2nii -nii ${subj}/NAPBL00/images/preventad_${subj}_NAPBL00_bold_001.mnc /converted_files/sub-${subj}/ses-1/func/sub-${subj}_ses-1_task-rest_acq_bold.nii
+    echo "--------------------------------"
 
+    echo "Testing if the subject has a PRE visit"
+    mnc2nii -nii ${subj}/PREBL00/images/preventad_${subj}_PREBL00_bold_001.mnc /converted_files/sub-${subj}/ses-BL00/func/sub-${subj}_ses-BL00_task-rest_bold.nii || \
+    ( echo "There is no PRE visit. Checking for a NAP visit..." ; \
+    mnc2nii -nii ${subj}/NAPBL00/images/preventad_${subj}_NAPBL00_bold_001.mnc /converted_files/sub-${subj}/ses-BL00/func/sub-${subj}_ses-BL00_task-rest_bold.nii )
+
+    echo "--------------------------------"
     echo "Conversion - 12 months Functional"
-    mnc2nii -nii ${subj}/PREFU12/images/preventad_${subj}_PREFU12_bold_001.mnc /converted_files/sub-${subj}/ses-2/func/sub-${subj}_ses-2_task-rest_acq_bold.nii
-    mnc2nii -nii ${subj}/NAPFU12/images/preventad_${subj}_NAPFU12_bold_001.mnc /converted_files/sub-${subj}/ses-2/func/sub-${subj}_ses-2_task-rest_acq_bold.nii
-    
-    echo "Conversion done"
+    echo "--------------------------------"
 
-done
+    echo "Testing if the subject has a PRE visit"
+    mnc2nii -nii ${subj}/PREFU12/images/preventad_${subj}_PREFU12_bold_001.mnc /converted_files/sub-${subj}/ses-FU12/func/sub-${subj}_ses-FU12_task-rest_bold.nii || \
+    ( echo "There is no PRE visit. Checking for a NAP visit..." ; \
+    mnc2nii -nii ${subj}/NAPFU12/images/preventad_${subj}_NAPFU12_bold_001.mnc /converted_files/sub-${subj}/ses-FU12/func/sub-${subj}_ses-FU12_task-rest_bold.nii )
+    
+    echo "Conversion done for subject ${subj}! "
+    echo " "
+done #Don't forget to tell the loop to close!
